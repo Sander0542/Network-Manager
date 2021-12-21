38fde4e0ec0cafba16001158858190b35bd6c044
Sander Jochems <sanderjochems@hotmail.nl>
2021-12-21 20:25:32 +0100

    Remove welcome page

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,24 +1,21 @@
 <template>
-  <app-layout title="Dashboard">
-    <template #header>
-      <h2 class="h4 font-weight-bold">
-        Dashboard
-      </h2>
-    </template>
+    <app-layout title="Dashboard">
+        <template #header>
+            <h2 class="h4 font-weight-bold">
+                Dashboard
+            </h2>
+        </template>
 
-    <welcome />
-  </app-layout>
+    </app-layout>
 </template>
 
 <script>
-import { defineComponent } from "vue"
+import {defineComponent} from "vue"
 import AppLayout from "@/Layouts/AppLayout.vue"
-import Welcome from "@/Jetstream/Welcome.vue"
 
 export default defineComponent({
-  components: {
-    AppLayout,
-    Welcome
-  }
+    components: {
+        AppLayout
+    }
 });
 </script>
