f9fa8075aa0a30982a81483216b02eadab13ae47
Sander Jochems <sanderjochems@hotmail.nl>
2021-12-21 21:17:01 +0100

    Create Network model

---
+++ database/migrations/2021_12_21_195348_create_networks_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNetworksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('networks', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users', 'id');
+            $table->string('name');
+            $table->string('range', 49);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('networks');
+    }
+}
