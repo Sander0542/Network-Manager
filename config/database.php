347a02ee6a80dc0642723c93e0ad0accbd96474c
Sander Jochems <sanderjochems@hotmail.nl>
2021-12-25 13:28:26 +0100

    Remove sqlite database

--- config/database.php
+++ config/database.php
@@ -35,14 +35,6 @@ return [
 
     'connections' => [
 
-        'sqlite' => [
-            'driver' => 'sqlite',
-            'url' => env('DATABASE_URL'),
-            'database' => env('DB_DATABASE', database_path('database.sqlite')),
-            'prefix' => '',
-            'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
-        ],
-
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
